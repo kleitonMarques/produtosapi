1a42ab5bc3b592255f147450991f421b8b083495
Kleiton Marques <kleitonm.marques@gmail.com>
2025-08-14 00:20:38 -0300

    first commit

---
+++ src/test/java/br/com/kleitonmarques/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleitonmarques.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
